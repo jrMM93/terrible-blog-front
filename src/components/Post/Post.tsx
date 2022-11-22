29dc13180e78ee577ee0db9c7d8a9fad4765ee11
jrmm93 <jeremyatn@gmail.com>
2022-11-22 18:14:50 +0100

    feat: display posts excerpt using dompurify

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DOMPurify from 'dompurify'
 import './styles.scss'
 
 type PostProps = {
   slug: string
   title: string
   category: string
+  excerpt: string
 }
 
-const Post: React.FC<PostProps> = ({ slug, title, category }) => {
+const Post: React.FC<PostProps> = ({ slug, title, category, excerpt }) => {
+  function createExcerptHTML() {
+    // clean dirty excerpt to prevent XSS attacks
+    const cleanExcerpt = DOMPurify.sanitize(excerpt)
+    return { __html: cleanExcerpt }
+  }
+
   return (
     <article className='post'>
       <Link to={`/posts/${slug}`}>
         <h2 className='post-title'>{title}</h2>
         <div className='post-category'>{category}</div>
+        <p
+          className='post-excerpt'
+          dangerouslySetInnerHTML={createExcerptHTML()} // inject cleaned HTML
+        />
       </Link>
     </article>
   )
